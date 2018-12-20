19a7a90f8261d66699c479886e0a512ebd47356f
Michael Boreham <bor3ham@gmail.com>
2018-12-20 14:12:33 +1100

    Add placeholder for funcs and tests

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import reducer from './reducer.js'
 function createJarm(config) {
   const merged = {
     fetch: defaultFetch,
+    schema: {},
     ...config,
     reducer,
 
-    // jarm api
+    // jarm api - writing
     populate: function(data) {
       if (!Array.isArray(data)) {
         return Actions.populateData([data])
@@ -17,6 +18,23 @@ function createJarm(config) {
         return Actions.populateData(data)
       }
     },
+    create: function(newInstance) {},
+    update: function(type, id, changes) {},
+    discard: function(type, id) {},
+    commit: function(type, id) {},
+    save: function(type, id) {},
+    purge: function(type, id) {},
+    // jarm api - reading
+    get: function(store, type, id) {
+      let instance = merged.get_local(store, type, id)
+      instance = merged.retree_remote(store, instance)
+      return merged.annotate_status(store, instance)
+    },
+    get_local: function(store, type, id) {},
+    get_remote: function(store, type, id) {},
+    retree_local: function(store, instance) {},
+    retree_remote: function(store, instance) {},
+    annotate_status: function(store, instance) {},
   }
   merged.fetch = merged.fetch.bind(merged)
   return merged
