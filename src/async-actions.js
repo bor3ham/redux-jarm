3e00aaf6b41e04f4c48769acde195b40deb8be68
Michael Boreham <bor3ham@gmail.com>
2019-01-10 15:22:41 +1100

    Move some save logic

--- src/async-actions.js
+++ src/async-actions.js
@@ -1,5 +1,6 @@
 import uuidv4 from 'uuid/v4'
 import * as ReducerActions from './reducer-actions'
+import { instanceKey } from './utils.js'
 
 function create(newInstance) {
   return (dispatch, getState) => {
@@ -12,13 +13,26 @@ function create(newInstance) {
   }
 }
 
-function save(type, id, url, createIncludes, updateIncludes, fetchAction) {
+function save(instanceType, id, url, createIncludes, updateIncludes, fetchAction) {
   return (dispatch, getState) => {
     const storeState = getState()
-    let instanceData = (storeState.local[type] || {})[id]
-    // todo: assert instance exists
+    if (
+      instanceType in storeState.local === false
+      || id in storeState.local[instanceType] === false
+    ) {
+      throw 'Local instance not found'
+    }
+    const instanceData = storeState.local[instanceType][id]
+    const key = instanceKey(instanceType, id)
 
-    dispatch(ReducerActions.recordSaving(type, id))
+    if (storeState.pending[key]) {
+      throw 'Instance already pending save'
+    }
+    if (!storeState.committed[key]) {
+      dispatch(ReducerActions.commitLocalInstance(instanceType, id))
+    }
+
+    dispatch(ReducerActions.recordSaving(instanceType, id))
     return dispatch(fetchAction(url, {method: 'POST',}, instanceData)).then(response => {
       dispatch(ReducerActions.recordCreationSuccess(id, response.data.data))
       return response.data.data
@@ -31,7 +45,7 @@ function save(type, id, url, createIncludes, updateIncludes, fetchAction) {
       }
       // otherwise genuine error
       else {
-        dispatch(ReducerActions.recordCreationError(type, id, error.data ? error.data : error))
+        dispatch(ReducerActions.recordCreationError(instanceType, id, error.data ? error.data : error))
         throw error
       }
     })
