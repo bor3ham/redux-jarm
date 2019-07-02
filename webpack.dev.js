5bd08a97a239597db50715e376bb73186098b336
Michael Boreham <bor3ham@gmail.com>
2019-07-02 15:23:59 +1000

    'Upgrade' babel

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,6 +15,31 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          query: {
+            presets: [
+              '@babel/env',
+              '@babel/react',
+            ],
+            plugins: [
+              '@babel/proposal-class-properties',
+              ['@babel/proposal-decorators', {
+                decoratorsBeforeExport: true,
+              }],
+              '@babel/proposal-do-expressions',
+              '@babel/proposal-export-default-from',
+              '@babel/proposal-export-namespace-from',
+              '@babel/proposal-function-bind',
+              '@babel/proposal-function-sent',
+              '@babel/proposal-json-strings',
+              '@babel/proposal-logical-assignment-operators',
+              '@babel/proposal-nullish-coalescing-operator',
+              '@babel/proposal-numeric-separator',
+              '@babel/proposal-optional-chaining',
+              '@babel/proposal-throw-expressions',
+              '@babel/syntax-dynamic-import',
+              '@babel/syntax-import-meta',
+            ],
+          },
         },
       },
     ],
