0b41b4f956af77aae2447af4d809906b215d27bc
Michael Boreham <bor3ham@gmail.com>
2020-03-19 14:30:31 +1100

    Add unhandled rejection catcher

--- setup-jest.js
+++ setup-jest.js
@@ -1,1 +1,5 @@
 global.fetch = require('jest-fetch-mock')
+
+// uncomment this line to catch uncaught promise rejections
+// note that this will break jest when it actually catches an exception
+// process.on('unhandledRejection', up => { throw up })
